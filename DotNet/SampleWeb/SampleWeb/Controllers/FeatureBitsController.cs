896d730661aa61fdb4cffdbc673e5860d4b4d464
Doug Seelinger <doseelin@microsoft.com>
2018-08-15 13:46:45 -0400

    Initial Application

---
+++ DotNet/SampleWeb/SampleWeb/Controllers/FeatureBitsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using FeatureBits.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeatureBitsController : ControllerBase
+    {
+        private readonly IFeatureBitEvaluator _evaluator;
+
+        public FeatureBitsController(IFeatureBitEvaluator evaluator)
+        {
+            _evaluator = evaluator;
+        }
+
+        [HttpGet("/{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var definition = _evaluator.Definitions.SingleOrDefault(d => d.Id == id);
+
+            if (definition != null)
+            {
+                bool isEnabled = _evaluator.IsEnabled((Features) id, 0);
+                return new JsonResult(isEnabled);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ DotNet/SampleWeb/SampleWeb/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FeatureBits.Core;
+using FeatureBits.Data;
+using FeatureBits.Data.AzureTableStorage;
+using FeatureBits.Data.EF;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SampleWeb
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json",
+                    optional: false,
+                    reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string featureBitsConnectionString = Configuration.GetConnectionString("FeatureBitsDbContext");
+            services.AddDbContext<FeatureBitsEfDbContext>(options => options.UseSqlServer(featureBitsConnectionString));
+            services.AddTransient<IFeatureBitsRepo, FeatureBitsEfRepo>((serviceProvider) =>
+            {
+                DbContextOptionsBuilder<FeatureBitsEfDbContext> options = new DbContextOptionsBuilder<FeatureBitsEfDbContext>();
+                options.UseSqlServer(featureBitsConnectionString);
+                var context = new FeatureBitsEfDbContext(options.Options);
+                return new FeatureBitsEfRepo(context);
+            });
+            services.AddTransient<IFeatureBitEvaluator, FeatureBitEvaluator>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
